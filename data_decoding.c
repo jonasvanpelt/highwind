4668ffbe481b8b73b5b4162a0e451155a15ad10b
Jonas Van Pelt <jonas@jonas-MacBookPro.(none)>
2013-08-02 14:51:41 +0200

    dd

---
+++ data_decoding.c
@@ -0,0 +1,246 @@
+#include <stdio.h>
+
+#include "data_decoding.h"
+
+#ifndef DEBUG 
+#define DEBUG 3
+#endif
+
+/*int main(int argc, char *argv[])
+{
+	int i;
+	
+	//fill output buffer with random test data
+	for(i=0;i<7;i++)
+	{
+		output.raw[i]=i; //creating random message
+	}
+			
+	uint8_t test[34];
+	
+	//encode test data buffer in test
+	data_encode(test);
+	
+	//print encoded test data
+	for(i=0;i<34;i++)
+	{
+		printf("%d ", test[i]);
+	}
+	printf("\n");
+	
+	switch_read_write();
+	
+	if(data_update(test)!=-1)
+	{ 
+		printf("succeded\n");
+		//try print the sended data as a struct
+		printf("servo command 1: %d\n",data->groundstation.commands.message.servo_commands[0]);
+		printf("servo command 2: %d\n",data->groundstation.commands.message.servo_commands[1]);
+		printf("servo command 3: %d\n",data->groundstation.commands.message.servo_commands[2]);
+		char* time_string;
+		time_string = ctime(&(data->groundstation.commands.message.time));
+		printf("time: %s",time_string);
+		//printf("timeval: %d\n",data->groundstation.commands.message.tv);
+		printf("new data: %d\n",data->groundstation.commands.message.new_data);	
+	}
+	else {
+		printf("failed\n");
+	}
+	
+	//print struct
+	
+	return 0;
+}*/
+
+void init_decoding(){
+	#if DEBUG
+		printf("Entering init_decoding\n");
+	#endif
+	
+	read_data = &ping;
+	write_data = &pong;
+}
+
+void switch_read_write()
+{
+	#if DEBUG
+		printf("Entering switch_read_write\n");
+	#endif
+	
+	struct Data* temp = read_data;
+
+	read_data = write_data;
+	write_data = temp;
+}
+
+int data_update(uint8_t stream[])
+{
+	#if DEBUG
+		printf("Entering data_update\n");
+	#endif
+	
+	int i = 0; 
+	uint8_t checksum_1 = 0;
+	uint8_t checksum_2 = 0;
+	uint8_t length = stream[1];
+	uint8_t sender = stream[2];	
+
+	//check first bit is 0x99
+	if(stream[0] != 0x99)
+	{
+		//unknown package !!!
+		return -1;
+	}
+	
+	//check checksums
+	for (i=1;i<length-2;i++)
+	{
+		checksum_1 += stream[i];
+		checksum_2 += checksum_1;
+	}
+	
+	//checksum1 is voorlaatste byte, checksu2 is last byte
+	if(checksum_1 != stream[length-2] || checksum_2 != stream[length-1] || sender == 0)
+	{
+
+		return -1;
+	}
+	
+	
+	return data_decode(0, sender, stream, length);
+}
+
+int data_decode(uint32_t pos, uint8_t sender,uint8_t stream[], int length) // start = 0
+{
+	#if DEBUG
+		printf("Entering data_decode\n");
+	#endif
+	
+	if(pos == 0)
+	{
+		pos = 3;
+	} 
+		
+	switch(sender)
+	{
+		
+		case 1: //sender_id of groundstation
+			switch(stream[pos])
+			{
+				case 1: // servo commands - 28 bytes
+					pos = data_write(stream, write_data->groundstation.commands.raw, 28, pos);
+					write_data->groundstation.commands.message.new_data = 0;
+					break;
+				default: return -1; break;
+			}
+		break;
+		case 2: //sender_id of beaglebone
+			switch(stream[pos])
+			{
+				case 1: // status - x bytes
+					pos = data_write(stream, write_data->bone_plane.status.raw, 8, pos);
+					write_data->bone_plane.status.message.new_data = 0;
+					break;
+				case 2: // error - 1 byte
+					pos = data_write(stream, write_data->bone_plane.error.raw, 1, pos);
+					write_data->bone_plane.error.message.new_data = 0;
+					break;
+				default: return -1; break;
+			}
+		break;
+		case 165: //sender_id of lisa
+			switch(stream[pos]) // the message id of the folowing message
+			{
+				case 25: // Svinfo - NOG DOEN
+					pos = data_write(stream, write_data->lisa_plane.svinfo.raw, 8, pos);
+					write_data->lisa_plane.svinfo.message.new_data = 0;
+					break;
+				case 54: // airspeed - 48 bytes
+					pos = data_write(stream, write_data->lisa_plane.airspeed.raw, 8, pos);
+					write_data->lisa_plane.airspeed.message.new_data = 0;
+					break;
+				case 57: // airspeed_ets - 40 bytes
+					pos = data_write(stream, write_data->lisa_plane.airspeed_ets.raw, 8, pos);
+					write_data->lisa_plane.airspeed_ets.message.new_data = 0;
+					break;
+				case 155: // gps_int - 88 bytes
+					pos = data_write(stream, write_data->lisa_plane.gps_int.raw, 8, pos);
+					write_data->lisa_plane.gps_int.message.new_data = 0;
+					break;
+				case 203: // imu_gyro_raw - 48 bytes
+					pos = data_write(stream, write_data->lisa_plane.imu_gyro_raw.raw, 12, pos);
+					write_data->lisa_plane.imu_gyro_raw.message.new_data = 0;
+					break;
+				case 204: // imu_accel_raw - 48 bytes
+					pos = data_write(stream, write_data->lisa_plane.imu_accel_raw.raw, 12, pos);
+					write_data->lisa_plane.imu_accel_raw.message.new_data = 0;
+					break;
+				case 205: // imu_mag_raw - 48 bytes
+					pos = data_write(stream, write_data->lisa_plane.imu_mag_raw.raw, 12, pos);
+					write_data->lisa_plane.imu_mag_raw.message.new_data = 0;
+					break;
+				case 221: // baro_raw - 40 bytes
+					pos = data_write(stream, write_data->lisa_plane.baro_raw.raw, 8, pos);
+					write_data->lisa_plane.baro_raw.message.new_data = 0;
+					break;
+				default: return -1;break;
+			}
+		 break;
+				default: return -1; break;
+	}
+	if (pos == length-2)
+	{
+		return 0; //data encoding succeeded
+	} else {
+		
+		return data_decode(pos, sender, stream, length);
+	}
+}
+
+int data_write(uint8_t stream[],uint8_t transfer[], int length, int pos)
+{
+	#if DEBUG
+		printf("Entering data_write\n");
+	#endif
+	
+	int i;
+	for(i=0;i<length;i++)
+	{
+	    transfer[i] = stream[++pos]; //pos is first time message id so first ++ for first data byte
+	}
+	
+	return pos + 1;
+}
+
+int data_encode(char buffer[])
+{
+	#if DEBUG
+		printf("Entering data_encode\n");
+	#endif
+	
+	int i = 0; 
+	int checksum_1 = 0;
+	int checksum_2 = 0;
+	uint8_t length = 34; // with header and checksum
+	uint8_t sender = 1;	
+	
+	buffer[0] = 0x99;
+	buffer[1] = length;
+	buffer[2] = 1; // sender id of server
+	buffer[3] = 1; // message id
+
+	for(i=0;i<28;i++)
+	{
+		buffer[4+i] = output.message[i];
+	}
+
+	for (i=2;i<length - 2;i++)
+	{
+		checksum_1 += buffer[i];
+		checksum_2 += checksum_1;
+	}
+	
+	buffer[32] = checksum_1;
+	buffer[33] = checksum_2;
+}
+
---
+++ data_decoding.h
@@ -0,0 +1,181 @@
+#ifndef DATA_DECODING_H_ 
+#define DATA_DECODING_H_
+
+#include <stdint.h>
+#include <time.h>
+#include <sys/time.h>
+
+/********************************
+ * GLOBALS
+ * ******************************/
+
+//OUTPUT BUFFER
+union Output{
+	int32_t raw[7];
+	uint8_t message[28]; // number of servos times 4
+} output;
+
+//INPUT
+struct Data {
+	//only for debugging, to send output commands to myself to test 
+	struct Groundstation { // sender id = 1
+		union Commands{ // id = 1
+			uint8_t raw[64];// always use sizeof
+			struct Commands_message{
+				int32_t servo_commands[7];
+				time_t time; // size 8 bytes
+				struct timeval tv; // size 16 bytes
+				int8_t new_data;
+			} message;
+		} commands;
+	} groundstation;
+	//for status and error messages coming from beaglebone on plane
+	struct Bone_plane { // sender id = 2
+		union Status { // id = 1
+			uint8_t raw[155];
+			struct Status_message {
+				uint8_t error;
+				uint8_t sd_state;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} status;
+		union Error { // id = 2
+			uint8_t raw[1];
+			struct Error_message {
+				uint8_t error;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} error;
+	} bone_plane;
+	//to decode error messages coming from lisa
+	struct Lisa_plane { // sender id = 165
+		union Airspeed { // id = 54
+			uint8_t raw[9];
+			struct Airspeed_message {
+				float airspeed;
+				float airspeed_sp;
+				float airspeed_cnt;
+				float groundspeed_sp;
+				time_t time; 
+				struct timeval tv; 
+				int8_t new_data;
+			} message;
+		} airspeed;
+		union Svinfo { // id = 25
+			uint8_t raw[33];
+			struct Svinfo_message {
+				uint8_t chn;
+				uint8_t svid;
+				uint8_t flags;
+				uint8_t qi;
+				uint8_t cno;
+				int8_t elev;
+				uint16_t azim;
+				time_t time; 
+				struct timeval tv; 
+				int8_t new_data;
+			} message;
+		} svinfo;
+		union Airspeed_ets { // id = 57
+			uint8_t raw[9];
+			struct Airspeed_ets_message {
+				uint16_t adc;
+				uint16_t offset;
+				float scaled;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} airspeed_ets;
+		union Gps_int { // id = 155
+			uint8_t raw[9];
+			struct Gps_int_message {
+				int32_t ecef_x;
+				int32_t ecef_y;
+				int32_t ecef_z;
+				int32_t lat;
+				int32_t lon;
+				int32_t alt;
+				int32_t hmsl;
+				int32_t ecef_xd;
+				int32_t ecef_yd;
+				int32_t ecef_zd;
+				int32_t pacc;
+				int32_t sacc;
+				uint32_t tow;
+				uint16_t pdop;
+				uint8_t numsv;
+				uint8_t fix;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} gps_int;
+		union Baro_raw { // id = 221
+			uint8_t raw[9];
+			struct Baro_raw_message {
+				int32_t abs;
+				int32_t diff;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} baro_raw;
+		union Imu_gyro_raw { // id = 203
+			uint8_t raw[9];
+			struct Imu_gyro_message {
+				int32_t gp;
+				int32_t gq;
+				int32_t gr;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} imu_gyro_raw;
+		union Imu_accel_raw { // id = 204
+			uint8_t raw[9];
+			struct Imu_accel_message {
+				int32_t ax;
+				int32_t ay;
+				int32_t az;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} imu_accel_raw;
+		union Imu_mag_raw { // id = 205
+			uint8_t raw[9];
+			struct Imu_mag_message {
+				int32_t mx;
+				int32_t my;
+				int32_t mz;
+				time_t time;
+				struct timeval tv;
+				int8_t new_data;
+			} message;
+		} imu_mag_raw;
+	} lisa_plane;
+};
+
+//one writes to ping and another can read data from pong and upside down
+struct Data ping;
+struct Data pong;
+
+//pointers to ping and pong
+struct Data* read_data;
+struct Data* write_data;
+
+/********************************
+ * PROTOTYPES
+ * ******************************/
+void init_decoding(void);
+int data_update(uint8_t stream[]);
+int data_decode(uint32_t pos, uint8_t sender,uint8_t stream[], int length);
+int32_t data_write(uint8_t stream[], uint8_t data[], int length, int pos);
+void switch_read_write(void);
+
+#endif /*DATA_DECODING_H_*/
