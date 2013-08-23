ee32a112c169bf01e558dab90b57d89a11be8e88
jonas <jonas@jonas-MacBookPro.(none)>
2013-08-23 17:02:32 +0200

    ;kjk;

--- runBeagle.sh
+++ runBeagle.sh
@@ -2,6 +2,11 @@
 clear
 gcc main_beaglebone.c udp_communication.c uart_communication.c log.c circular_buffer.c data_decoding.c  -pthread -DDEBUG=0 -o main_beaglebone
 
+# Milan
+#./main_beaglebone 10.33.132.47 8888 7777
+
+# Jonas
+ ./main_beaglebone 10.33.136.11 8888 7777
 #jonas
 #./main_beaglebone 10.33.136.11 8888 7777
 
