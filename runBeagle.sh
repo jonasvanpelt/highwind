a4166c19c357f3a933f6a287772a7f6dcb2e4fe5
jonas <jonas@jonas-MacBookPro.(none)>
2013-08-28 07:27:03 +0200

    change runBeagle

--- runBeagle.sh
+++ runBeagle.sh
@@ -8,10 +8,10 @@ gcc main_beaglebone.c udp_communication.c uart_communication.c log.c circular_bu
 # Jonas
 # ./main_beaglebone 10.33.136.11 8888 7777
 #jonas
-#./main_beaglebone 10.33.136.11 8888 7777
+./main_beaglebone 10.33.136.11 8888 7777
 
 #carousel2
-./main_beaglebone 10.33.136.31 8888 7777
+#./main_beaglebone 10.33.136.31 8888 7777
 
 
 
