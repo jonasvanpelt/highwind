b9ca065d6f8a61b3ff0f40599b4040d255a2b54e
jonas <jonas@jonas-MacBookPro.(none)>
2013-08-29 15:31:18 +0200

    change runWind.sh

--- runWind.sh
+++ runWind.sh
@@ -4,6 +4,6 @@ clear
 gcc main_windsensor.c udp_communication.c uart_communication.c log.c circular_buffer.c data_decoding.c -DDEBUG=0 -o main_wind
 
 # Jonas
-./main_wind 10.33.136.11 6666
+./main_wind 10.33.136.11 8888
 
 
